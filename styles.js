55ff13cc3df46d7753fcaaf5697008dc2c90269f
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-01-17 08:21:25 +0300

    Updated Css

--- styles.js
+++ styles.js
@@ -1,3 +1,9 @@
+function pizza(size,crust,topping){
+    this.size=size
+    this.crust=crust
+    this.topping=topping
+}
+
 $(document).ready(function(){
     $("form-group#pizza").submit(function(event){
         event.preventDefault()
@@ -7,5 +13,12 @@ $(document).ready(function(){
         let selectedPizzaTopping = $("select#barbecue-steak").val()
 
         let pizza = pizza(selectedPizzaSize,selectedPizzaTopping,selectedPizzaCrust)
+
+        $("ul#pizzas").append("<li><span class='pizzas'>" + pizza.size + "</span></li>")
+
+        $("select#pizza-size").val()
+        $("select#pizza-crust").val()
+        $("select#barbecue-steak").val()
+
     })
 })
